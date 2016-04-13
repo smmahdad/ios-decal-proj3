3fd08197e0badf4a5aa2f973c3a990a735ffa403
Sam Mahdad <smmahdad@Sams-MacBook-Pro.local>
2016-04-12 18:24:40 -0700

    Basically done. Think I just need to check if the like has to persist

--- Photos/Photos/DetailedPhotoViewController.swift
+++ Photos/Photos/DetailedPhotoViewController.swift
@@ -18,20 +18,48 @@ class DetailedPhotoViewController: UIViewController {
     
     @IBOutlet weak var likesLabel: UILabel!
     
+    @IBOutlet weak var likeButton: UIButton!
+    
     var image : UIImage?
     var numLikes : Int?
-    var liked : Bool?
+    var liked : Bool? = false
     var userName : String?
     var date : String?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.likeButton.addTarget(self, action: "buttonTapped", forControlEvents: .TouchUpInside)
+        
         imageView.image = self.image!
-        likesLabel.text = String(self.numLikes!) + " likes"
+        likesLabel.text = String(self.numLikes!)
         //Deal with liked picture
         userNameLabel.text = self.userName!
-        dateLabel.text = self.date!
+        
+        var tempStr = String(NSDate(timeIntervalSince1970: Double(self.date!)!))
+        var ind = tempStr.startIndex.advancedBy(5)
+        tempStr = tempStr.substringFromIndex(ind)
+        ind = tempStr.startIndex.advancedBy(5)
+        dateLabel.text = tempStr.substringToIndex(ind)
+        if self.liked! {
+            likeButton.setImage(UIImage(named: "filledHeart.jpg"), forState: .Normal)
+            self.likesLabel.text = String(self.numLikes! + 1)
+        }
+        print(dateLabel.text)
+    }
+    
+    func buttonTapped() {
+        self.liked! = !(self.liked!)
+        if self.liked! {
+            likeButton.setImage(UIImage(named: "filledHeart.jpg"), forState: .Normal)
+            self.likesLabel.text = String(self.numLikes! + 1)
+        } else {
+            likeButton.setImage(UIImage(named: "emptyHeart.jpg"), forState: .Normal)
+            likesLabel.text = String(self.numLikes!)
+        }
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
